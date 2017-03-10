35bc77622a7ee25d6e6a941405e73cdd28b921e7
Firman Cahyandi <f.cahyandi@gmail.com>
2017-03-10 15:27:59 +1100

    website layout done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'pages#main'
+	get 'partials/nav'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

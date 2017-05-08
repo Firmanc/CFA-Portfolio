d67c53d9046626c7e9ad28f68325d385986f67e3
Firman Cahyandi <f.cahyandi@gmail.com>
2017-05-08 13:43:35 +1000

    create a download resume button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def main
+
   end
+
+  def download
+      send_file "#{Rails.root}/app/assets/images/resume.pdf", :type=>"application/pdf", :x_sendfile=>true
+  end
+
 end
